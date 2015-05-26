db7140ba1e9d6424adfce17779fa65c8c7b6194f
Ruslan Cheshko <chex@soul.li>
2015-05-26 09:48:22 +0300

    fix bug in palindrome

--- lec1/p06.erl
+++ lec1/p06.erl
@@ -1,16 +1,23 @@
 -module(p06).
 -export([is_palindrome/1]).
 
-%
-%  AHTUNG. We have a bug! :'(
-%
 
 is_palindrome(Palindrome)->
-  PartLen = (p04:len(Palindrome)-1) div 2,
-  reverse_first_and_compare_with_last(Palindrome, PartLen).
+  PartLen = p04:len(Palindrome) div 2,
+  {LastEstimated, LastFact} = slice_and_reverse_palindrome(Palindrome, PartLen),
+  parts_is_palindrome(LastEstimated, LastFact).
 
-reverse_first_and_compare_with_last([H|T], N)->
-  rfacwl(T, N-1, [H]).
 
-rfacwl([_|T], 0, FI)-> FI == T;
-rfacwl([H|T], N, FI)-> rfacwl(T, N-1, [H|FI]).
+slice_and_reverse_palindrome([H|T], PartLen)->
+  slice_and_reverse_palindrome(T, PartLen-1, [H]).
+
+slice_and_reverse_palindrome(Tail, 0, Acc)->
+  {Acc, Tail};
+
+slice_and_reverse_palindrome([H|T], PartLen, Acc)->
+  slice_and_reverse_palindrome(T, PartLen-1, [H|Acc]).
+
+
+parts_is_palindrome([X|Y], [_|[X|Y]])-> true;
+parts_is_palindrome([X|Y], [X|Y])-> true;
+parts_is_palindrome(_, _)-> false.
